fd6d90773caa62719f989c09ac6a46881a8b64ae
Moritz Gellner <moritz.gellner@gmail.com>
2014-04-14 22:45:44 -0500

    twitter bugfixes

--- app/tasks/sample.py
+++ app/tasks/sample.py
@@ -4,6 +4,8 @@ from app.tasks import celery
 
 from socialscraper.socialscraper import twitter
 
+from celery import chord, group, chain, signature
+
 @celery.task(name='addition')
 def addition(x, y):
 	return x + y
@@ -13,10 +15,20 @@ def scrape_followers(username):
 	scraper = twitter.TwitterScraper()
 	scraper.add_user_info(os.getenv("TWITTER_USERNAME"),os.getenv('TWITTER_PASSWORD'))
 	followers = []
+	subtasks = []
 	for follower in scraper.get_followers('aljohri'):
-		# do something
+		#print follower.screen_name
+		sig = celery.send_task('scrape.twitter.feed', args=[follower.screen_name], queue='celery')
+		subtasks.append(sig)
 		followers.append(follower)
+
 	return str(followers)
+
+@celery.task(name='scrape.twitter.feed')
+def scrape_feed(username):
+	scraper = twitter.TwitterScraper()
+	tweets = scraper.get_feed_by_screen_name(username)
+	return str(tweets)
 
 
 @celery.task(name='scrape')
