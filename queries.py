2c21e70ff1048a06b2cc9da4f46f1e8a79cb362a
Daniel Thirman <Dthirman@gmail.com>
2014-05-14 19:04:41 -0500

    Updated queries.py to generate bit strings

--- queries.py
+++ queries.py
@@ -57,34 +57,52 @@ def currentcity(cityList=[], unknown=None):
     """
     if unknown:
         query = FacebookUser.query.filter_by(currentcity=None)
+    elif(len(cityList) > 0):
+        try:
+            test = int(cityList[0])
+            zipString = ""
+            for zi in cityList:
+                zipString = zipString + " OR zipcode = "+str(zi)+""
+            zipString= zipString[4:]
+            query = FacebookUser.query.from_statement("select * from facebook_users where lower(currentcity) in (select distinct name from locations where type = 'currentcity' and (" + zipString + "))")
+        except ValueError: 
+            cityString = ""
+            for city in cityList:
+                cityString = cityString + "OR currentcity Like '%"+city+"%'"
+            cityString= cityString[2:]
+            query = FacebookUser.query.from_statement("Select * from facebook_users where " + cityString)
     else:
-        cityString = ""
-        for city in cityList:
-            cityString = cityString + "OR currentcity Like '%"+city+"%'"
-        cityString= cityString[2:]
-        query = FacebookUser.query.from_statement("Select * from facebook_users where " + cityString)
-
+        return []
     return query.all()
 
 
-def hometown(city=[], unknown=None):
+def hometown(cityList=[], unknown=None):
     """
     Queries the database for people who are between a certain age range
 
     Ignores all people who don't have a birth year set because their birth year is set to 2014 and the query will only query up to january 1st on the current year
 
     """
-
     if unknown:
         query = FacebookUser.query.filter_by(hometown=None)
+    elif(len(cityList)>0):
+            try:
+                test = int(cityList[0])
+                zipString = ""
+                for zi in cityList:
+                    zipString = zipString + " OR zipcode = "+str(zi)+""
+                zipString= zipString[4:]
+                query = FacebookUser.query.from_statement("select * from facebook_users where lower(hometown) in (select distinct name from locations where type = 'currentcity' and (" + zipString + "))")
+            except ValueError: 
+                cityString = ""
+                for city in cityList:
+                    cityString = cityString + "OR hometown Like '%"+city+"%'"
+                cityString= cityString[2:]
+                query = FacebookUser.query.from_statement("Select * from facebook_users where " + cityString)
     else:
-        cityString = ""
-        for city in cityList:
-            cityString = cityString + "OR hometown Like '%"+city+"%'"
-        cityString= cityString[2:]
-        query = FacebookUser.query.from_statement("Select * from facebook_users where " + cityString)
-
+        return []
     return query.all()
+
 
 
 def collegeInList(collegeList=[], unknown=None):
@@ -173,16 +191,16 @@ def employerInList(employerList=[], unknown=None):
     Ignores all people who don't have a birth year set because their birth year is set to 2014 and the query will only query up to january 1st on the current year
 
     """
-
     if unknown:
         query = FacebookUser.query.filter_by(employer=None)
-    else:
+    elif len(employerList)>0:
         employerString = ""
         for employer in employerList:
             employerString = employerString + "OR employer Like '%"+employer+"%'"
         employerString= employerString[2:]
         query = FacebookUser.query.from_statement("Select * from facebook_users where " + employerString)
-
+    else:
+        return []
     return query.all()
 
 def andQuery(query1, query2):
@@ -219,15 +237,29 @@ def orQuery(query1, query2):
         
 def buildTree(depth = 0, funcArray = [], query=[], printString = ""):
     line = printString + " : " + str(len(query))
-    print line 
+    # print line 
     f.write(line + "\n")
     if depth < len(funcArray) and (depth == 0 or len(query) > 0):
-        for x in funcArray[depth][2]:
-            if depth ==0:
-                buildTree(depth +1, funcArray, funcArray[depth][0](x), printString+str(x[0]))
-            else:
-                buildTree(depth +1, funcArray, andQuery(query, funcArray[depth][0](x)), printString+", " + str(x[0]))
-        
+        if depth ==0:
+            for x in funcArray[depth][2]:
+                buildTree(depth +1, funcArray, funcArray[depth][0](x[1:]), funcArray[depth][1] + ": " + str(x[0]))
+            buildTree(depth +1, funcArray, funcArray[depth][0](unknown=True), funcArray[depth][1] + ": Unknown" )
+        else:
+            for x in funcArray[depth][2]:   
+                buildTree(depth +1, funcArray, andQuery(query, funcArray[depth][0](x[1:])), printString+", " + funcArray[depth][1] + ": " + str(x[0]))
+            buildTree(depth +1, funcArray, andQuery(query, funcArray[depth][0](unknown=True)), printString+", " + funcArray[depth][1] + ": Unknown" )
+    elif len(query)>0:
+        print printString + " Count : " + str(len(query))
+        for q in query:
+            bitstring = ""
+            for i in range(len(funcArray)):
+                seg = printString.split(",")[i]
+                cat = seg.split(":")[1][1:]
+                cats = map(lambda x: x[0], funArray[i][2])+["Unknown"]
+                pos = cats.index(cat)
+                bitstring += "0"*pos + "1"+ ("0"*(len(cats)-pos-1))
+            line = str(q.uid) +": "+ bitstring
+            f2.write(line+"\n")
 
 
 def permutations(istr, cstr, used_indices):
@@ -244,26 +276,50 @@ def fixQuote(x):
     else:
         return x
 
+def readEmploy():
+    employArray= []
+    with open('Employment.csv', 'rb') as c:
+        creader = csv.reader(c, delimiter=',')
+        firstLine = True
+        for row in creader:
+            if firstLine:
+                firstLine = False
+            else:    
+                for i in range(1,len(row)):
+                    if row[i] != '':
+                        line = filter(lambda x: x != "'", row[i])
+                        if i>=len(employArray):
+                            employArray.append([line])
+                        else:
+                            employArray[i-1].append(line)
+    return employArray
 
-employArray= []
+def readZip():
+    zipArray= []
+    with open('Location.tsv', 'rb') as c:
+        creader = csv.reader(c, delimiter='\t')
+        firstLine = True
+        for row in creader:
+            if firstLine:
+                firstLine = False
+            else:    
+                zipArray.append(row[0])
+    return zipArray
 
-with open('Employment.csv', 'rb') as c:
-    creader = csv.reader(c, delimiter=',')
-    firstLine = True
-    for row in creader:
-        if firstLine:
-            firstLine = False
-        else:    
-            for i in range(1,len(row)):
-                if row[i] != '':
-                    line = filter(lambda x: x != "'", row[i])
-                    if i>=len(employArray):
-                        employArray.append([line])
-                    else:
-                        employArray[i].append(line)
+employCat = map(lambda x: x[0], readEmploy())+["Unknown"]
 
-funArray = [[employerInList, "Employer" , employArray], [birthday, "Age", [[15,24], [25,34], [35,44], [45, 54], [55, 64], [65, 200]]] , [sex, "Sex", ["m", "f", "o"]], [currentcity, "Current Location", [["Illinois"], ["New York"], ["California"]]]]
-perm = permutations([0, 1, 2, 3] , [], set())
+funEmploy = [employerInList, "Employer" , readEmploy()]
+funAge = [birthday, "Age", [["15-24", 15,24], ["25-34", 25,34], ["35-44", 35,44], ["45-54", 45, 54], ["55-64", 55, 64], ["65+", 65, 200]]]
+funSex = [sex, "Sex", ["Mm", "Ff", "Oo"]]
+funCurrCity  = [currentcity, "Current Location", [readZip(), ["Illinois"]]]
+funHomeTown  = [hometown, "Hometown", [readZip(), ["Illinois"]]]
+funArray = [funEmploy, funAge , funSex, funCurrCity, funHomeTown]
+perm = permutations([0, 1] , [], set())
 f = open("result.txt", 'w')
-for p in perm:
-    buildTree(funcArray=map(lambda x: funArray[x], p))
+f2 = open("bitarrays.txt", 'w')
+# q = hometown(["Illinois"])
+# print q[0].uid
+buildTree(funcArray=funArray)
+# print currentCityZipInList([60091, 60076, 60093])
+# for p in perm:
+#     buildTree(funcArray=map(lambda x: funArray[x], p))
