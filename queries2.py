6ff83c840684a5adf61f44e52490f2484020d907
Al Johri <al.johri@gmail.com>
2014-05-19 13:59:30 -0500

    Make queries2.py importable in shell / ipython.

--- queries2.py
+++ queries2.py
@@ -5,10 +5,9 @@ from sqlalchemy import or_, and_
 from datetime import datetime
 from dateutil import parser
 
-###################################################################
-
-# Queries
-
+######################################################
+# Dynamic Queries
+######################################################
 
 def sex(sex=None, unknown=False):
     if unknown: filtr = FacebookUser.sex == None
@@ -32,11 +31,33 @@ def currentcity(city=None, unknown=False):
         filtr = FacebookUser.currentcity.ilike("%%%s%%" % city)
     return filtr
 
+def currentCityInList(cityList=[], unknown=False):
+    if unknown: filtr = currentcity(unknown=True)
+    elif len(cityList) <1: filtr=None
+    elif type(cityList[0]) == int:
+        or_list = [zipcode(x, "currentcity") for x in cityList]
+        filtr = or_(*or_list)
+    else:
+        or_list = [currentcity(x) for x in cityList]
+        filtr = or_(*or_list)
+    return filtr
+
 def hometown(city=None, unknown=False):
     if unknown: 
         filtr = FacebookUser.hometown == None
     else:
         filtr = FacebookUser.hometown.ilike("%%%s%%" % city)
+    return filtr
+
+def hometownInList(cityList=[], unknown=False):
+    if unknown: filtr = hometown(unknown=True)
+    elif len(cityList) <1: filtr= None
+    elif type(cityList[0]) == int:
+        or_list = [zipcode(x, "hometown") for x in cityList]
+        filtr = or_(*or_list)
+    else:
+        or_list = [hometown(x) for x in cityList]
+        filtr = or_(*or_list)
     return filtr
 
 def zipcode(zipcode=None, typeCity=None, unknown=False):
@@ -51,6 +72,14 @@ def employer(employer=None, unknown=False):
     if unknown: filtr = FacebookUser.employer == None
     else: filtr = FacebookUser.employer.ilike("%%%s%%" % employer)
 
+    return filtr
+
+def employerInList(employerList=[], unknown=False):
+    if unknown: filtr = employer(unknown=True)
+    elif len(employerList) <1: filtr=FacebookUser.employer=="fsdfsdfsdfsdfdsfsdfsdfdsdsfsd"
+    else:
+        or_list = [employer(x) for x in employerList]
+        filtr = or_(*or_list)
     return filtr
 
 def school(school=None, unknown=False):
@@ -73,110 +102,11 @@ def college(college=None, unknown=False):
 
     return filtr
 
-def highschool(college=None, unknown=False):
-    if unknown: filtr = FacebookUser.highschool == None
-    else: filtr = FacebookUser.highschool.ilike("%%%s%%" % highschool)
-
-    return filtr
-
-def likes(unknown=False):
-    if unknown: filtr = FacebookUser.pages == None
-    else: filtr = FacebookUser.pages != None
-
-    return filtr
-
-###################################################################
-
-# # Examples
-
-# # Example 1
-# print "Example 1: Querying people of age 20 to 30"
-# query = FacebookUser.query.filter(
-#             age(age=(20,30))
-#         )
-# print map(lambda user: (user.username, user.birthday), query.all())
-# print "\n"
-
-# # Example 2
-# print "Example 2: Querying people of age 20 to 30 who are male"
-# query = FacebookUser.query.filter(
-#     and_(
-#         age(age=(20,30)),
-#         sex(sex='m')
-#     )
-# )
-# print map(lambda user: (user.username, user.birthday, user.sex), query.all())
-# print "\n"
-
-# # Example 3
-# print "Example 3: Querying people of age 20 to 30 who are male and from evanston"
-# query = FacebookUser.query.filter(
-#     and_(
-#         age(age=(20,30)),
-#         sex(sex='m'),
-#         city('evanston')
-#     )
-# )
-# print map(lambda user: (user.username, user.birthday, user.sex, user.currentcity, user.hometown), query.all())
-# print "\n"
-
-# # Example 4
-# print "Example 4: People who have zipcode 60201"
-# query = FacebookUser.query.filter(
-#     zipcode('60201')
-# )
-# print map(lambda user: (user.username, map(lambda location: (location.type, location.zipcode) ,user.locations)), query.all())
-# print "\n"
-
-# # Example 5
-# print "Example 5: People who's employer is microsoft"
-# query = FacebookUser.query.filter(
-#     employer('microsoft')
-# )
-# print map(lambda user: (user.username, user.employer), query.all())
-# print "\n"
-
-# # Example 6
-# print "Example 6: People who's college is Northwestern"
-# query = FacebookUser.query.filter(
-#     college('northwestern')
-# )
-# print map(lambda user: (user.username, user.college), query.all())
-# print "\n"
-
-# print "------------------------------------------------------------"
-
-# ###################################################################
-
-# Iterate through above queries
-
-def employerInList(employerList=[], unknown=False):
-    if unknown: filtr = employer(unknown=True)
-    elif len(employerList) <1: filtr=FacebookUser.employer=="fsdfsdfsdfsdfdsfsdfsdfdsdsfsd"
+def collegeInList(schoolList=[], unknown=False):
+    if unknown: filtr = college(unknown=True)
+    elif len(schoolList) <1: filtr= None
     else:
-        or_list = [employer(x) for x in employerList]
-        filtr = or_(*or_list)
-    return filtr
-
-def currentCityInList(cityList=[], unknown=False):
-    if unknown: filtr = currentcity(unknown=True)
-    elif len(cityList) <1: filtr=None
-    elif type(cityList[0]) == int:
-        or_list = [zipcode(x, "currentcity") for x in cityList]
-        filtr = or_(*or_list)
-    else:
-        or_list = [currentcity(x) for x in cityList]
-        filtr = or_(*or_list)
-    return filtr
-
-def hometownInList(cityList=[], unknown=False):
-    if unknown: filtr = hometown(unknown=True)
-    elif len(cityList) <1: filtr= None
-    elif type(cityList[0]) == int:
-        or_list = [zipcode(x, "hometown") for x in cityList]
-        filtr = or_(*or_list)
-    else:
-        or_list = [hometown(x) for x in cityList]
+        or_list = [college(x) for x in schoolList]
         filtr = or_(*or_list)
     return filtr
 
@@ -188,12 +118,16 @@ def highSchoolInList(schoolList=[], unknown=False):
         filtr = or_(*or_list)
     return filtr
 
-def collegeInList(schoolList=[], unknown=False):
-    if unknown: filtr = college(unknown=True)
-    elif len(schoolList) <1: filtr= None
-    else:
-        or_list = [college(x) for x in schoolList]
-        filtr = or_(*or_list)
+def highschool(college=None, unknown=False):
+    if unknown: filtr = FacebookUser.highschool == None
+    else: filtr = FacebookUser.highschool.ilike("%%%s%%" % highschool)
+
+    return filtr
+
+def likes(unknown=False):
+    if unknown: filtr = FacebookUser.pages == None
+    else: filtr = FacebookUser.pages != None
+
     return filtr
 
 def age(age = [0, 10000], unknown=False):
@@ -212,9 +146,9 @@ def age(age = [0, 10000], unknown=False):
     
     return filtr
 
-###################################################################
-
-# Code mildly modified from original queries.py
+######################################################
+# Parse CSV/TSV files
+######################################################
 
 def readEmploy():
     employArray= []
@@ -246,81 +180,93 @@ def readZip():
                 zipArray.append(row[0])
     return [zipArray[0]] + map(int, zipArray[1:])
 
+if __name__ == "__main__":
 
-# Still need read AGE, read LIKES
+    """
+    Example Usage of Dynamic Queries
+    --------------------------------
+    query = FacebookUser.query.filter(age(age=(20,30)))
+    query = FacebookUser.query.filter(and_(age(age=(20,30)),sex('m')))
+    query = FacebookUser.query.filter(and_(age(age=(20,30)),sex('m'),currentcity('evanston')))
+    query = FacebookUser.query.filter(zipcode('60201'))
+    query = FacebookUser.query.filter(employer('microsoft'))
+    query = FacebookUser.query.filter(college('northwestern'))
+    """
 
-funEmploy = [employerInList, "Employer" , readEmploy()]
-funAge = [age, "Age", [["15-24", 15,24], ["25-34", 25,34], ["35-44", 35,44], ["45-54", 45, 54], ["55-64", 55, 64], ["65+", 65, 200]]]
-funSex = [sex, "Sex", ["Mm", "Ff", "Oo"]]
-funCurrentCity  = [currentCityInList, "Current City", [readZip(), ["Illinois", "Illinois"]]]
-funHometown  = [hometownInList, "Hometown", [readZip(), ["Illinois", "Illinois"]]]
+    # Still need read AGE, read LIKES
 
-funArray = [funEmploy, funAge , funSex, funCurrentCity, funHometown]
+    funEmploy = [employerInList, "Employer" , readEmploy()]
+    funAge = [age, "Age", [["15-24", 15,24], ["25-34", 25,34], ["35-44", 35,44], ["45-54", 45, 54], ["55-64", 55, 64], ["65+", 65, 200]]]
+    funSex = [sex, "Sex", ["Mm", "Ff", "Oo"]]
+    funCurrentCity  = [currentCityInList, "Current City", [readZip(), ["Illinois", "Illinois"]]]
+    funHometown  = [hometownInList, "Hometown", [readZip(), ["Illinois", "Illinois"]]]
 
-def buildTree(depth = 0, funcArray = [], filters=None, printString = ""):
+    funArray = [funEmploy, funAge , funSex, funCurrentCity, funHometown]
 
-    if filters is not None: 
-        users = FacebookUser.query.filter(filters).all()
-        length = len(users)
-    else: 
-        users = []
-        length = 0
+    def buildTree(depth = 0, funcArray = [], filters=None, printString = ""):
 
-    line = printString + " : " + str(length)
-    print line 
-    # f.write(line + "\n")
+        if filters is not None: 
+            users = FacebookUser.query.filter(filters).all()
+            length = len(users)
+        else: 
+            users = []
+            length = 0
 
-    if depth < len(funcArray) and (depth == 0 or length > 0):
+        line = printString + " : " + str(length)
+        print line 
+        # f.write(line + "\n")
 
-        if depth == 0:
+        if depth < len(funcArray) and (depth == 0 or length > 0):
 
-            for x in funcArray[depth][2]:
+            if depth == 0:
+
+                for x in funcArray[depth][2]:
+                    buildTree(
+                        depth + 1, 
+                        funcArray, 
+                        funcArray[depth][0](x[1:]), 
+                        funcArray[depth][1] + ": " + str(x[0])
+                    )
+
                 buildTree(
-                    depth + 1, 
+                    depth +1, 
                     funcArray, 
-                    funcArray[depth][0](x[1:]), 
-                    funcArray[depth][1] + ": " + str(x[0])
+                    funcArray[depth][0](unknown=True), 
+                    funcArray[depth][1] + ": Unknown" 
                 )
 
-            buildTree(
-                depth +1, 
-                funcArray, 
-                funcArray[depth][0](unknown=True), 
-                funcArray[depth][1] + ": Unknown" 
-            )
+            else:
 
-        else:
+                for x in funcArray[depth][2]:   
+                    buildTree(depth +1, funcArray, 
+                        and_(
+                            filters, 
+                            funcArray[depth][0](x[1:])
+                        ), 
+                        printString + ", " + funcArray[depth][1] + ": " + str(x[0])
+                    )
 
-            for x in funcArray[depth][2]:   
                 buildTree(depth +1, funcArray, 
                     and_(
                         filters, 
-                        funcArray[depth][0](x[1:])
+                        funcArray[depth][0](unknown=True)
                     ), 
-                    printString + ", " + funcArray[depth][1] + ": " + str(x[0])
+                    printString + ", " + funcArray[depth][1] + ": Unknown" 
                 )
 
-            buildTree(depth +1, funcArray, 
-                and_(
-                    filters, 
-                    funcArray[depth][0](unknown=True)
-                ), 
-                printString + ", " + funcArray[depth][1] + ": Unknown" 
-            )
+        elif length>0:
+            # print printString + " Count : " + str(length)
+            for q in users:
+                bitstring = ""
+                for i in range(len(funcArray)):
+                    seg = printString.split(",")[i]
+                    cat = seg.split(":")[1][1:]
+                    cats = map(lambda x: x[0], funcArray[i][2])+["Unknown"]
+                    pos = cats.index(cat)
+                    bitstring += "0"*pos + "1"+ ("0"*(len(cats)-pos-1))
+                line = str(q.uid) +": "+ bitstring
+                # f2.write(line+"\n")
 
-    elif length>0:
-        # print printString + " Count : " + str(length)
-        for q in users:
-            bitstring = ""
-            for i in range(len(funcArray)):
-                seg = printString.split(",")[i]
-                cat = seg.split(":")[1][1:]
-                cats = map(lambda x: x[0], funcArray[i][2])+["Unknown"]
-                pos = cats.index(cat)
-                bitstring += "0"*pos + "1"+ ("0"*(len(cats)-pos-1))
-            line = str(q.uid) +": "+ bitstring
-            # f2.write(line+"\n")
-
-for i in range(len(funArray)):
-    buildTree(funcArray=funArray[i:])
+    for i in range(len(funArray)):
+        buildTree(funcArray=funArray[i:])
 
